d6044085fd5e5cb71067bd114bc08136d5b95e5b
csly2023 <csly.2023@gmail.com>
2024-05-30 21:04:34 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/', [WelcomeController::class, 'index'])->name('home');
+Route::get('/about-us', [WelcomeController::class, 'about'])->name('about');
+Route::get('/contact', [WelcomeController::class, 'contact'])->name('contact');
