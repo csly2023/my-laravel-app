a9aa595d028fa3f12df6eb8ad8270e171d52fdfe
csly2023 <csly.2023@gmail.com>
2024-06-06 20:07:07 +0600

    make full name - class 19

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+@section('title')
+    About Page
+@endsection
+@section('body')
+    <section class="py-5 bg-secondary-subtle">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>This is about page</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
